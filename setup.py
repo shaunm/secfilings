b15a8e0ef282976a02a823153cc7f8576377d392
sman1 <developer@ngtapps.com>
2018-05-20 16:03:18 -0400

    v2.0

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(name='secfilings',
-      version='0.1',
+      version='0.2',
       description='A simple utility to get some SEC forms for companies that file.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  
       url='http://github.com/sman1/secfilings',
       author='NGT Apps',
       author_email='developer@ngtapps.com',
